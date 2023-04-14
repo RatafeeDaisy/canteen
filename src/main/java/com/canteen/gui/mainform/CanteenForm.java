0f03c910330cee65a5c3d6d8da1e8c6b8b47d839
s303student <s303@student.com>
2023-04-14 09:34:23 +0800

    更新主菜单界面

--- src/main/java/com/canteen/gui/mainform/CanteenForm.java
+++ src/main/java/com/canteen/gui/mainform/CanteenForm.java
@@ -6,6 +6,9 @@ package com.canteen.gui.mainform;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.*;
+import org.jdesktop.beansbinding.*;
+import org.jdesktop.beansbinding.AutoBinding.UpdateStrategy;
 
 /**
  * @author 10047
@@ -19,6 +22,13 @@ public class CanteenForm {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         canteenForm = new JFrame();
         scrollPane1 = new JScrollPane();
+        mainTable = new JTable();
+        mainLable = new JLabel();
+        mainButton_menu = new JButton();
+        mainButton_order = new JButton();
+        mainButton_customer = new JButton();
+        mainButton_printer = new JButton();
+        mainButton_onekey = new JButton();
 
         //======== canteenForm ========
         {
@@ -26,8 +36,82 @@ public class CanteenForm {
             canteenForm.setTitle("\u98df\u5802\u8ba2\u5355\u7ba1\u7406\u7cfb\u7edf");
             var canteenFormContentPane = canteenForm.getContentPane();
             canteenFormContentPane.setLayout(null);
+
+            //======== scrollPane1 ========
+            {
+
+                //---- mainTable ----
+                mainTable.setModel(new DefaultTableModel(
+                    new Object[][] {
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                        {null, null, null, null, null, null},
+                    },
+                    new String[] {
+                        "\u4e3b\u83dc", "\u914d\u83dc1", "\u914d\u83dc2", "\u914d\u83dc3", "\u5355\u4ef7", "\u4efd\u6570"
+                    }
+                ));
+                mainTable.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 12));
+                scrollPane1.setViewportView(mainTable);
+            }
             canteenFormContentPane.add(scrollPane1);
-            scrollPane1.setBounds(180, 45, 575, 265);
+            scrollPane1.setBounds(180, 60, 535, 280);
+
+            //---- mainLable ----
+            mainLable.setText("\u4eca\u65e5\u83dc\u5355");
+            mainLable.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 20));
+            canteenFormContentPane.add(mainLable);
+            mainLable.setBounds(400, 15, 110, 35);
+
+            //---- mainButton_menu ----
+            mainButton_menu.setText("\u83dc\u5355\u8bbe\u7f6e");
+            canteenFormContentPane.add(mainButton_menu);
+            mainButton_menu.setBounds(50, 60, 95, 35);
+
+            //---- mainButton_order ----
+            mainButton_order.setText("\u8ba2\u5355\u7ba1\u7406");
+            canteenFormContentPane.add(mainButton_order);
+            mainButton_order.setBounds(50, 120, 95, 35);
+
+            //---- mainButton_customer ----
+            mainButton_customer.setText("\u987e\u5ba2\u7ba1\u7406");
+            canteenFormContentPane.add(mainButton_customer);
+            mainButton_customer.setBounds(50, 180, 95, 35);
+
+            //---- mainButton_printer ----
+            mainButton_printer.setText("\u6253\u5355\u8bbe\u7f6e");
+            canteenFormContentPane.add(mainButton_printer);
+            mainButton_printer.setBounds(50, 240, 95, 35);
+
+            //---- mainButton_onekey ----
+            mainButton_onekey.setText("\u4e00\u952e\u6253\u5355");
+            canteenFormContentPane.add(mainButton_onekey);
+            mainButton_onekey.setBounds(50, 300, 95, 35);
 
             {
                 // compute preferred size
@@ -43,8 +127,8 @@ public class CanteenForm {
                 canteenFormContentPane.setMinimumSize(preferredSize);
                 canteenFormContentPane.setPreferredSize(preferredSize);
             }
-            canteenForm.setSize(790, 410);
-            canteenForm.setLocationRelativeTo(canteenForm.getOwner());
+            canteenForm.setSize(800, 420);
+            canteenForm.setLocationRelativeTo(null);
         }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
@@ -52,5 +136,12 @@ public class CanteenForm {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     private JFrame canteenForm;
     private JScrollPane scrollPane1;
+    private JTable mainTable;
+    private JLabel mainLable;
+    private JButton mainButton_menu;
+    private JButton mainButton_order;
+    private JButton mainButton_customer;
+    private JButton mainButton_printer;
+    private JButton mainButton_onekey;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
